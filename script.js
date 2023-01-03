251fe31e3a633e996b811866914897d9f426d100
João "Kyle" Bernardo <joaoluiss22@outlook.com>
2023-01-03 01:19:27 -0300

    header

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const btnMobile = document.getElementById('btn-mobile');
+const btnCloseMobile = document.getElementById('btn-close');
 
 function toggleMenu() {
-  const menu = document.getElementById('menus');
-  menu.classList.toggle('active');
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnCloseMobile.addEventListener('click', toggleMenu);
+
